a964118233a496681a274d408939230c89a46aa1
shizukani9 <73003469+shizukani9@users.noreply.github.com>
2020-10-18 17:06:57 -0400

    Update JavaApplication16.java

--- JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -18,7 +18,10 @@ public class JavaApplication16 {
      */
     public static void main(String[] args){
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null,"Hola mundo");
+        JOptionPane.showMessageDialog(null,"Hola mundo"); //4
+        JOptionPane.showMessageDialog(null,"1"); //1
+        JOptionPane.showMessageDialog(null,"2"); //2
+        JOptionPane.showMessageDialog(null,"3"); //3
     }
     
 }
